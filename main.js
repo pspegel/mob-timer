054c504a1e846b616ae1d29c989da0e6b5afbcca
Peter Spegel <peter.spegel.umea@gmail.com>
2019-04-19 17:03:50 +0200

    Fixes hard resets.

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const electron = require('electron');
-const electronReload = require('electron-reload');
-const url = require('url');
-const path = require('path');
+const path = require("path");
+const electron = require("electron");
+require("electron-reload")(__dirname, {
+  electron: path.join(__dirname, "node_modules", ".bin", "electron")
+});
+const url = require("url");
 
 const { app, BrowserWindow, ipcMain } = electron;
 
 let extraWindows = [];
 
-electronReload(__dirname);
-
 let mainWindow;
 
-ipcMain.on('time-has-elapsed', (event, arg) => {
+ipcMain.on("time-has-elapsed", (event, arg) => {
   mainWindow.show();
   mainWindow.maximize();
 
   blockExternalDisplays();
 });
 
-ipcMain.on('timer-started', (event, arg) => {
+ipcMain.on("timer-started", (event, arg) => {
   setTimeout(function() {
     if (mainWindow.isVisible()) {
       mainWindow.minimize();
@@ -37,25 +37,29 @@ ipcMain.on('timer-started', (event, arg) => {
   }, 3000);
 });
 
-app.on('ready', function() {
-  mainWindow = new BrowserWindow({icon: path.join(__dirname, '/favicon.ico')});
+app.on("ready", function() {
+  mainWindow = new BrowserWindow({
+    icon: path.join(__dirname, "/favicon.ico")
+  });
 
-  mainWindow.on('close', function() {
+  mainWindow.on("close", function() {
     app.quit();
   });
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'mainWindow.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
   mainWindow.maximize();
 });
 
 function blockExternalDisplays() {
   extraWindows = [];
   const displays = electron.screen.getAllDisplays();
-  const primaryDisplayId = electron.screen.getPrimaryDisplay().id
+  const primaryDisplayId = electron.screen.getPrimaryDisplay().id;
   for (const i in displays) {
     if (displays[i].id === primaryDisplayId) {
       continue;
@@ -66,14 +70,15 @@ function blockExternalDisplays() {
       x: externalDisplay.bounds.x + 50,
       y: externalDisplay.bounds.y + 50
     });
-    
-    extraWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'extraWindow.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+
+    extraWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "extraWindow.html"),
+        protocol: "file:",
+        slashes: true
+      })
+    );
     extraWindow.maximize();
     extraWindows.push(extraWindow);
   }
 }
-
